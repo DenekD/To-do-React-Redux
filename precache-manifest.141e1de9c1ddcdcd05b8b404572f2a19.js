1a233baa60e9d3bd16850e679d3f1119d8eec302
Barnaba <Denek20@gmail.com>
2020-05-18 18:15:28 +0200

    Updates

---
+++ precache-manifest.141e1de9c1ddcdcd05b8b404572f2a19.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce2643c53106d06ff0a2da557b385d84",
+    "url": "/To-do-React-Redux/index.html"
+  },
+  {
+    "revision": "d03c6602ef2959e44931",
+    "url": "/To-do-React-Redux/static/css/main.0d19045e.chunk.css"
+  },
+  {
+    "revision": "bdaa266f06e0279796cd",
+    "url": "/To-do-React-Redux/static/js/2.d6b43418.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/To-do-React-Redux/static/js/2.d6b43418.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d03c6602ef2959e44931",
+    "url": "/To-do-React-Redux/static/js/main.dc340aee.chunk.js"
+  },
+  {
+    "revision": "e36e82de8a099487dc44",
+    "url": "/To-do-React-Redux/static/js/runtime-main.b7779a04.js"
+  },
+  {
+    "revision": "3f307db3432acccec34dd3c809ece3a6",
+    "url": "/To-do-React-Redux/static/media/checkedIcon.3f307db3.svg"
+  },
+  {
+    "revision": "b9e65c667436d5b68b4d286eebd07861",
+    "url": "/To-do-React-Redux/static/media/desk.b9e65c66.jpg"
+  },
+  {
+    "revision": "87da38651727cea7140bf328302a5c61",
+    "url": "/To-do-React-Redux/static/media/removeIcon.87da3865.svg"
+  }
+]);
--- src/actiontypes/task.js
+++
@@ -1,3 +0,0 @@
-export const  COMPLITE_TASK = 'task/COMPLITE_TASK';
-export const  ADD_TASK = 'task/ADD_TASK';
-export const  REMOVE_TASK = 'task/REMOVE_TASK';
